87962ce33239e6ae890a115b952b6426c625470d
strinsx <bert2rafales@gmail.com>
2024-03-13 05:55:36 +0800

    updates..

--- Astral/Assets/scripts/PlayerCombat Rafales.cs
+++ Astral/Assets/scripts/PlayerCombat Rafales.cs
@@ -12,6 +12,7 @@ public class PlayerCombat : MonoBehaviour
     public GameObject attackpoint;
     public float radius;
     public LayerMask enemies;
+    public float KBForce = 5;
 
     private void Awake()
     {
@@ -43,14 +44,20 @@ public class PlayerCombat : MonoBehaviour
 
         Collider2D[] enemy = Physics2D.OverlapCircleAll(attackpoint.transform.position, radius, enemies);
 
-        foreach (Collider2D enemyGameobject in enemy)
+        foreach (Collider2D enemyCollider in enemy)
         {
-            
             Debug.Log("HIT ENEMY");
-          enemyGameobject.GetComponent<MinionHealth>().health -= 10;
-        }
+            Rigidbody2D enemyRigidbody = enemyCollider.gameObject.GetComponent<Rigidbody2D>();
+            if (enemyRigidbody != null)
+            {
+                // Determine knockback direction based on relative position of enemy to player
+                Vector2 knockbackDirection = (enemyRigidbody.position - GetComponent<Rigidbody2D>().position).normalized;
+                enemyRigidbody.AddForce(knockbackDirection * KBForce, ForceMode2D.Impulse);
+            }
 
-        } 
+            enemyCollider.GetComponent<MinionHealth>().health -= 10;
+        }
+    } 
 
     private void OnDrawGizmos()
     {
