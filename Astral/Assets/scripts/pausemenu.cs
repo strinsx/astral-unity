47bab50d9c4505dc191e114c07a2df7be8b716f4
Ric_j <113926448+Suaiso@users.noreply.github.com>
2024-03-16 23:25:45 +0800

    PausemenuDone

---
+++ Astral/Assets/scripts/pausemenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour
+{
+
+    private bool isPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+    }
+}
